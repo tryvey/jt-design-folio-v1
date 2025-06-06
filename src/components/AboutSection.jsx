70140baaefe3e7b095bdcc3351ef16893c8eddd0
Jamie Treyvaud <tryvey@gmail.com>
2025-06-06 16:20:46 +1000

    About section
    Aligned to the left

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection({ about }) {
   return (
     <section className="w-full bg-neutral-950 text-white py-16 px-4 md:px-0 mx-auto">
       <div className="flex flex-col max-w-3xl mx-auto">
-        <div className="prose prose-invert max-w-none">
+        <div className="prose prose-invert max-w-none text-left">
           <ReactMarkdown>{about}</ReactMarkdown>
         </div>
       </div>
